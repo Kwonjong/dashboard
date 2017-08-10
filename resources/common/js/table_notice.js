fcfc9f1d7ac5b8b6ffa2f4b86c7e04e9bc7e6866
DESKTOP-AR9MVM8\WJ.Kim <kwonjong865@gmail.com>
2017-08-10 15:35:34 +0900

    8.10 작업내역 - 통합게시판(공지사항, 자주하는질문)에 대한 페이지 작업 : 리스트, 폼, 뷰

---
+++ resources/common/js/table_notice.js
@@ -0,0 +1,32 @@
+$(function(){
+	$('#notice_list').DataTable({
+		"dom":
+			"<'row'<'data_search'f>>" +
+			"<'row'<'col-sm-12'tr>>" +
+			"<'row'<'data_paging'p>>"+
+			"<'row'<'btn_set'>>",
+		"columnDefs":[
+			{"targets":[0, 1, 2], "orderable" : false}
+		],
+		"order":[]
+	});
+
+	$('div.btn_set').html('<button type="button" id="add_notice" class="btn btn-warning">추가</button> <button type="button" id="del_notice" class="btn btn-danger">삭제</button>');
+
+	$('#all_select').click(function(){
+		if($('#all_select').prop('checked')){
+			$('input[name=one_select]').prop('checked',true);
+		} else{
+			$('input[name=one_select]').prop('checked',false);
+		}
+	});
+
+	$('#add_notice').click(function(){
+		$(location).attr('href', 'notice_form.html');	
+	});
+
+	$('#del_notice').click(function(){
+		// 삭제 버튼을 클릭하였을 때의 액션을 입력해주세요.
+	});
+
+})
